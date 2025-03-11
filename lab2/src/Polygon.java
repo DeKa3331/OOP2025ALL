d993ecf8cb02f7a9618593c2121033234f61d856
Jakub <jakub.deka.dobrowolski@gmail.com>
2025-03-11 22:40:28 +0100

    zajecia 2 mgr niedziolka, juz dziala

---
+++ lab2/src/Polygon.java
@@ -0,0 +1,58 @@
+
+public class Polygon {
+    private final Point[] vertices;
+
+    // konstruktor dokonuje głębokiej kopii tablicy
+    public Polygon(Point[] vertices) {
+        this.vertices = new Point[vertices.length];
+        for(int i=0; i<vertices.length; i++){
+            this.vertices[i] = new Point(vertices[i]);
+        }
+    }
+    // konstruktor kopiujący (głęboka kopia)
+    public Polygon(Polygon other) {
+        this.vertices = new Point[other.vertices.length];
+        for(int i=0; i<other.vertices.length; i++){
+            this.vertices[i] = new Point(other.vertices[i]);
+        }
+    }
+
+    public void setPoint(int ix, int x, int y){
+        this.vertices[ix].setX(x);
+        this.vertices[ix].setY(y);
+    }
+
+    public BoundingBox boundingBox(){
+        if(vertices.length == 0){
+            return null;
+        }
+        double minX = vertices[0].getX();
+        double maxX = vertices[0].getX();
+        double minY = vertices[0].getY();
+        double maxY = vertices[0].getY();
+        for(int i=1; i<vertices.length; i++){
+            if(vertices[i].getX() < minX) minX = vertices[i].getX();
+            if(vertices[i].getX() > maxX) maxX = vertices[i].getX();
+            if(vertices[i].getY() < minY) minY = vertices[i].getY();
+            if(vertices[i].getY() > maxY) maxY = vertices[i].getY();
+        }
+        return new BoundingBox(minX, minY, maxX-minX, maxY-minY);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for(Point p : vertices) {
+            s.append(p.getX()).append(",").append(p.getY()).append(" ");
+        }
+        return s.toString().trim();
+    }
+
+    public String toSvg() {
+        //TODO: dodać klasę Style z polami fill, stroke, stroke_width
+        //TODO: dodać pole style w tej klasie i zamienić atrybut style poniżej
+        return "<polygon points=\""+
+                this
+                +"\" style=\"fill:none;stroke:purple;stroke-width:3\" />";
+    }
+}
