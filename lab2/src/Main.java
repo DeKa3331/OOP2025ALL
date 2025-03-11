9f894e48fb2aeb53c052f3fa1f5e2a105f0b57ca
Jakub <jakub.deka.dobrowolski@gmail.com>
2025-03-11 22:38:42 +0100

    zajecia 2 mgr niedziolka

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -1,15 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.IOException;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //TODO: Utworzyć obiekt Style i przypisać do utworzonych polygonów (referencja)
+        //TODO: zmienić style w main i zaobserwować zmianę we wszystkich wielokątach
+
+        Point p1 = new Point(30 ,40);
+        Point p2 = new Point();
+        p2.setY(40);
+        System.out.println(p1);
+        System.out.println(p2);
+
+        Segment seg = new Segment(p1, p2);
+        System.out.println(seg);
+        p2.setX(60);
+        System.out.println(seg);
+
+        Point[] arr = {p1, p2, p1};
+        arr[2] = new Point(30, 0);
+//        p2.setX(0);
+
+        Polygon poly = new Polygon(arr);
+        System.out.println(poly);
+        Polygon polyCopy = new Polygon(poly);
+        System.out.println(polyCopy);
+
+        poly.setPoint(1, 0, 40);
+        System.out.println("Poly: "+poly);
+        System.out.println("Kopia: "+polyCopy);
+
+        System.out.println(polyCopy.toSvg());
+
+
+        SvgScene scene = new SvgScene();
+        scene.addPolygon(poly);
+        scene.addPolygon(polyCopy);
+        polyCopy.setPoint(0, -50, -50);
+        polyCopy.setPoint(1, 0, 0);
+        polyCopy.setPoint(2, -100, -30);
+
+        Polygon square = new Polygon(new Point[]{
+                new Point(70, 70),
+                new Point(130, 70),
+                new Point(130, 130),
+                new Point(70, 130)
+        });
+        scene.addPolygon(square);
+
+        System.out.println("Scena svg:");
+        System.out.println(scene.toSvg());
+
+        System.out.println(polyCopy.boundingBox());
+
+        scene.save("rysunek.svg");
     }
+
+
 }
