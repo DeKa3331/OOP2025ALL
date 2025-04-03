2349aed8ed378fc490035dc7e463d1f945b0a0bf
Jakub <jakub.deka.dobrowolski@gmail.com>
2025-04-03 15:52:33 +0200

    NegativeLifespanException  added

---
+++ lab5/src/NegativeLifespanException.java
@@ -0,0 +1,7 @@
+public class NegativeLifespanException extends Exception{
+    public NegativeLifespanException(Person p)
+    {
+        super(p+ " has negative lifespan");
+    }
+
+}
--- lab5/src/Person.java
+++ lab5/src/Person.java
@@ -1,20 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDate;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoField;
+import java.util.*;
 //import java.util.TreeSet;
 
 public class Person implements Comparable<Person>{
+
     private final String name, surname;
     private final LocalDate birth;
-    private final Set<Person> children; //= new TreeSet<>();
+    private final LocalDate death;
+    private final Set<Person> children;
 
-    public Person(String name, String surname, LocalDate birth) {
+
+    public Person(String name, String surname, LocalDate birth,LocalDate death) throws NegativeLifespanException {
         this.name = name;
         this.surname = surname;
         this.birth = birth;
+        this.death = death;
         this.children = new HashSet<>();
+        if(death!= null && birth.isAfter(death))
+        {
+            throw new NegativeLifespanException(this);
+        }
     }
 
     public boolean adopt(Person p) {
@@ -48,6 +61,7 @@ public class Person implements Comparable<Person>{
                 "name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
                 ", birth=" + birth +
+                ", death=" + death +
                 '}';
     }
 
@@ -58,5 +72,61 @@ public class Person implements Comparable<Person>{
 
     public String getFullName() {
         return name + ' ' + surname;
+    }
+    public static Person fromCsvline(String csvLine) throws NegativeLifespanException {
+        String[] elements = csvLine.split(",", -1);
+        String[] fullName = elements[0].split(" ");
+        LocalDate birth = LocalDate.parse(elements[1], DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+        DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                .appendValue(ChronoField.DAY_OF_MONTH, 2)
+                .appendLiteral(".")
+                .appendValue(ChronoField.MONTH_OF_YEAR)
+                .appendLiteral(".")
+                .appendValue(ChronoField.YEAR, 4)
+                .toFormatter();
+        LocalDate death;
+        try {
+            death = LocalDate.parse(elements[2], formatter);
+        } catch (DateTimeParseException e){
+            death = null;
+        }
+        return new Person(fullName[0], fullName[1], birth, death);
+    }
+
+    public static List<Person> fromCSV(String csvFileName)
+    {
+        List<Person> personList=new ArrayList<>();
+        /*try {
+            FileReader fr =new FileReader(csvFileName);
+            BufferedReader br = new BufferedReader(fr);
+            System.out.println(br.readLine());
+            br.close();
+        }*/
+        try(BufferedReader br = new BufferedReader(new FileReader(csvFileName)))
+        {
+            br.readLine();
+            String line;
+            while((line=br.readLine()) != null)
+            {
+                Person readPerson= null;
+                try {
+                    readPerson = fromCsvline(line);
+                    personList.add(readPerson);
+                } catch (NegativeLifespanException e) {
+                    //ignorujemy i nic nie dodajemy
+                    //nie ma potrzeby przerywania wczytywania calego pliku
+                    System.err.println(e.getMessage());
+                }
+                personList.add(readPerson);
+            }
+
+        }
+//jesli chcielibysmy wczytywac do czasu bledu to tutaj moglibysmy wyrzucic to exception zamiast w petli
+        catch(IOException e)
+        {
+            System.err.println(e.getMessage());
+        }
+        return personList;
     }
 }
